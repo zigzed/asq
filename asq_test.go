0a250ff8be61551f6b827125ddb2a768c1dc5e67
Wilbur Lang <mr.wclong@yahoo.com>
2022-06-16 21:44:47 +0800

    test case for async Then

--- asq_test.go
+++ asq_test.go
@@ -158,176 +158,176 @@ func TestAsq(t *testing.T) {
 		is.NoErr(err)
 	}()
 
-	// _, err = app.SubmitTask(ctx, task.NewTask(nil, "testA"))
-	// is.NoErr(err)
-	// br()
+	_, err = app.SubmitTask(ctx, task.NewTask(nil, "testA"))
+	is.NoErr(err)
+	br()
 
-	// _, err = app.SubmitTask(ctx, task.NewTask(nil, "testB", 3))
-	// is.NoErr(err)
-	// br()
+	_, err = app.SubmitTask(ctx, task.NewTask(nil, "testB", 3))
+	is.NoErr(err)
+	br()
 
-	// ar, err := app.SubmitTask(ctx, task.NewTask(nil, "testC", 30))
-	// is.NoErr(err)
-	// is.NotNil(ar)
-	// var (
-	// 	valc int
-	// 	errc error
-	// )
-	// ok, err := ar.Wait(ctx, &valc)
-	// is.True(ok)
-	// is.Equal(valc, 60)
-	// is.NoErr(errc)
-	// fmt.Printf("testC result: %+v, %+v\n", valc, errc)
-	// br()
+	ar, err := app.SubmitTask(ctx, task.NewTask(nil, "testC", 30))
+	is.NoErr(err)
+	is.NotNil(ar)
+	var (
+		valc int
+		errc error
+	)
+	ok, err := ar.Wait(ctx, &valc)
+	is.True(ok)
+	is.Equal(valc, 60)
+	is.NoErr(errc)
+	fmt.Printf("testC result: %+v, %+v\n", valc, errc)
+	br()
 
-	// _, err = app.SubmitTask(ctx, task.NewTask(nil, "testD", simpleStruct{
-	// 	A: "a",
-	// 	B: 1,
-	// 	C: struct {
-	// 		M string
-	// 		N sql.NullString
-	// 		P *int
-	// 	}{
-	// 		M: "m",
-	// 		N: sql.NullString{Valid: true, String: "n"},
-	// 		P: new(int),
-	// 	},
-	// }))
-	// is.NoErr(err)
-	// br()
+	_, err = app.SubmitTask(ctx, task.NewTask(nil, "testD", simpleStruct{
+		A: "a",
+		B: 1,
+		C: struct {
+			M string
+			N sql.NullString
+			P *int
+		}{
+			M: "m",
+			N: sql.NullString{Valid: true, String: "n"},
+			P: new(int),
+		},
+	}))
+	is.NoErr(err)
+	br()
 
-	// testEv = 5
-	// v := 2
-	// ar, err = app.SubmitTask(ctx,
-	// 	task.NewTask(
-	// 		task.NewTaskOption(3, 2*time.Second).WithResultExpired(2*time.Minute),
-	// 		"testE",
-	// 		&v))
-	// is.NoErr(err)
-	// is.NotNil(ar)
-	// br()
+	testEv = 5
+	v := 2
+	ar, err = app.SubmitTask(ctx,
+		task.NewTask(
+			task.NewTaskOption(3, 2*time.Second).WithResultExpired(2*time.Minute),
+			"testE",
+			&v))
+	is.NoErr(err)
+	is.NotNil(ar)
+	br()
 
-	// var (
-	// 	vale simpleStruct
-	// )
-	// ok, err = ar.Wait(ctx, &vale)
-	// is.True(ok)
-	// is.Err(err)
-	// fmt.Printf("testE result: %v, %v, %v\n", vale, ok, err)
-	// br()
+	var (
+		vale simpleStruct
+	)
+	ok, err = ar.Wait(ctx, &vale)
+	is.True(ok)
+	is.Err(err)
+	fmt.Printf("testE result: %v, %v, %v\n", vale, ok, err)
+	br()
 
-	// testEv = 2
-	// ar, err = app.SubmitTask(ctx,
-	// 	task.NewTask(
-	// 		task.NewTaskOption(3, 2*time.Second).WithResultExpired(2*time.Minute),
-	// 		"testE",
-	// 		&v))
-	// ok, err = ar.Wait(ctx, &vale)
-	// is.NoErr(err)
-	// is.True(ok)
-	// is.Equal(vale.A, "result_A")
-	// is.Equal(vale.B, 0)
-	// is.Equal(vale.C.M, "result_M")
-	// is.NotNil(vale.C.P)
-	// is.Equal(*vale.C.P, v)
-	// fmt.Printf("testE result: %v, %v, %v\n", vale, ok, err)
-	// br()
+	testEv = 2
+	ar, err = app.SubmitTask(ctx,
+		task.NewTask(
+			task.NewTaskOption(3, 2*time.Second).WithResultExpired(2*time.Minute),
+			"testE",
+			&v))
+	ok, err = ar.Wait(ctx, &vale)
+	is.NoErr(err)
+	is.True(ok)
+	is.Equal(vale.A, "result_A")
+	is.Equal(vale.B, 0)
+	is.Equal(vale.C.M, "result_M")
+	is.NotNil(vale.C.P)
+	is.Equal(*vale.C.P, v)
+	fmt.Printf("testE result: %v, %v, %v\n", vale, ok, err)
+	br()
 
-	// ar, err = app.SubmitTask(ctx,
-	// 	task.NewTask(
-	// 		nil,
-	// 		"testF"))
-	// ok, err = ar.Wait(ctx)
-	// fmt.Printf("testF result: %v, %v\n", ok, err)
-	// br()
+	ar, err = app.SubmitTask(ctx,
+		task.NewTask(
+			nil,
+			"testF"))
+	ok, err = ar.Wait(ctx)
+	fmt.Printf("testF result: %v, %v\n", ok, err)
+	br()
 
-	// _, err = app.SubmitTask(ctx,
-	// 	task.NewTask(
-	// 		nil,
-	// 		"testA"))
-	// is.NoErr(err)
-	// br()
+	_, err = app.SubmitTask(ctx,
+		task.NewTask(
+			nil,
+			"testA"))
+	is.NoErr(err)
+	br()
 
-	// ar, err = app.SubmitTask(ctx,
-	// 	task.NewTask(nil, "testC", 300),
-	// 	task.NewTask(nil, "testB"))
-	// is.NoErr(err)
-	// ok, err = ar.Wait(ctx)
-	// is.NoErr(err)
-	// is.True(ok)
-	// br()
+	ar, err = app.SubmitTask(ctx,
+		task.NewTask(nil, "testC", 300),
+		task.NewTask(nil, "testB"))
+	is.NoErr(err)
+	ok, err = ar.Wait(ctx)
+	is.NoErr(err)
+	is.True(ok)
+	br()
 
-	// ar, err = app.SubmitTask(ctx,
-	// 	task.NewTask(nil, "testC", 500),
-	// 	task.NewTask(nil, "testC"),
-	// 	task.NewTask(nil, "testC"))
-	// is.NoErr(err)
-	// ok, err = ar.Wait(ctx, &valc)
-	// is.NoErr(err)
-	// is.True(ok)
-	// is.Equal(valc, 4000)
-	// br()
+	ar, err = app.SubmitTask(ctx,
+		task.NewTask(nil, "testC", 500),
+		task.NewTask(nil, "testC"),
+		task.NewTask(nil, "testC"))
+	is.NoErr(err)
+	ok, err = ar.Wait(ctx, &valc)
+	is.NoErr(err)
+	is.True(ok)
+	is.Equal(valc, 4000)
+	br()
 
-	// ar, err = app.SubmitTask(ctx,
-	// 	task.NewTask(nil, "testA"),
-	// 	task.NewTask(nil, "testC", 200),
-	// 	task.NewTask(nil, "testC"))
-	// is.NoErr(err)
-	// ok, err = ar.Wait(ctx, &valc)
-	// is.NoErr(err)
-	// is.True(ok)
-	// is.Equal(valc, 800)
-	// br()
+	ar, err = app.SubmitTask(ctx,
+		task.NewTask(nil, "testA"),
+		task.NewTask(nil, "testC", 200),
+		task.NewTask(nil, "testC"))
+	is.NoErr(err)
+	ok, err = ar.Wait(ctx, &valc)
+	is.NoErr(err)
+	is.True(ok)
+	is.Equal(valc, 800)
+	br()
 
-	// ar, err = app.SubmitTask(ctx,
-	// 	task.NewTask(nil, "testG", 1, 2, 3))
-	// is.NoErr(err)
-	// var x, y, z int
-	// ok, err = ar.Wait(ctx, &x, &y, &z)
-	// is.NoErr(err)
-	// is.True(ok)
-	// is.Equal(x, 2)
-	// is.Equal(y, 4)
-	// is.Equal(z, 6)
-	// br()
+	ar, err = app.SubmitTask(ctx,
+		task.NewTask(nil, "testG", 1, 2, 3))
+	is.NoErr(err)
+	var x, y, z int
+	ok, err = ar.Wait(ctx, &x, &y, &z)
+	is.NoErr(err)
+	is.True(ok)
+	is.Equal(x, 2)
+	is.Equal(y, 4)
+	is.Equal(z, 6)
+	br()
 
-	// ar, err = app.SubmitTask(ctx,
-	// 	task.NewTask(nil, "testG", 100, 200, 300),
-	// 	task.NewTask(nil, "testG"),
-	// 	task.NewTask(nil, "testG"))
-	// is.NoErr(err)
-	// ok, err = ar.Wait(ctx, &x, &y, &z)
-	// is.NoErr(err)
-	// is.True(ok)
-	// is.Equal(x, 800)
-	// is.Equal(y, 1600)
-	// is.Equal(z, 2400)
-	// br()
+	ar, err = app.SubmitTask(ctx,
+		task.NewTask(nil, "testG", 100, 200, 300),
+		task.NewTask(nil, "testG"),
+		task.NewTask(nil, "testG"))
+	is.NoErr(err)
+	ok, err = ar.Wait(ctx, &x, &y, &z)
+	is.NoErr(err)
+	is.True(ok)
+	is.Equal(x, 800)
+	is.Equal(y, 1600)
+	is.Equal(z, 2400)
+	br()
 
-	// ar, err = app.SubmitTask(ctx,
-	// 	task.NewTask(nil, "testH"))
-	// is.NoErr(err)
-	// var ts time.Time
-	// ok, err = ar.Wait(ctx, &ts)
-	// is.NoErr(err)
-	// is.True(ok)
-	// diff := time.Since(ts)
-	// fmt.Printf("testH timing: %.6f\n", diff.Seconds())
-	// br()
+	ar, err = app.SubmitTask(ctx,
+		task.NewTask(nil, "testH"))
+	is.NoErr(err)
+	var ts time.Time
+	ok, err = ar.Wait(ctx, &ts)
+	is.NoErr(err)
+	is.True(ok)
+	diff := time.Since(ts)
+	fmt.Printf("testH timing: %.6f\n", diff.Seconds())
+	br()
 
-	// ar, err = app.SubmitTask(ctx,
-	// 	task.NewTask(nil, "testI", "mm", 111))
-	// is.NoErr(err)
-	// var m string
-	// var n int
-	// ok, err = ar.Wait(ctx, &n, &m)
-	// is.NoErr(err)
-	// is.True(ok)
-	// is.Equal(m, "mm")
-	// is.Equal(n, 111)
-	// br()
+	ar, err = app.SubmitTask(ctx,
+		task.NewTask(nil, "testI", "mm", 111))
+	is.NoErr(err)
+	var m string
+	var n int
+	ok, err = ar.Wait(ctx, &n, &m)
+	is.NoErr(err)
+	is.True(ok)
+	is.Equal(m, "mm")
+	is.Equal(n, 111)
+	br()
 
-	ar, err := app.SubmitTask(ctx,
+	ar, err = app.SubmitTask(ctx,
 		task.NewTask(nil, "testG", 3, 6, 9))
 	is.NoErr(err)
 	ar.Then(ctx,
@@ -336,25 +336,26 @@ func TestAsq(t *testing.T) {
 			is.Equal(a, 3*2)
 			is.Equal(b, 6*2)
 			is.Equal(c, 9*2)
+			br()
 		},
 		func(err error) {
 			fmt.Printf("error: %v\n", err)
 		})
-	br()
 
 	ar, err = app.SubmitTask(ctx,
-		task.NewTask(nil, "testJ"))
+		task.NewTask(task.NewTaskOption(1, 1*time.Second), "testJ"))
 	is.NoErr(err)
 	ar.Then(ctx,
 		func() {
 			fmt.Printf("should not be called here\n")
+			br()
 		},
 		func(err error) {
 			fmt.Printf("then error: %v\n", err)
+			br()
 		})
-	br()
 
-	time.Sleep(30 * time.Second)
+	time.Sleep(3 * time.Second)
 }
 
 func br() {
